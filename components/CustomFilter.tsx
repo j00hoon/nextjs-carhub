eafb60690d9cdb6501972d49254160b66e8a5ec0
Seunghoon Baik <j00hoon1101@gmail.com>
2024-03-01 12:04:24 -0500

    Add custom filters which are fuel and year

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { CustomFilterProps } from '@/types';
+import { updateSearchParams } from '@/utils';
 import { Listbox, Transition } from '@headlessui/react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
@@ -10,13 +11,28 @@ import React, { Fragment, useState } from 'react';
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
 
+  const router = useRouter();
   const [selected, setSelected] = useState(options[0]);
+
+  const handleUpdateParams = (e: { title: string, value: string}) => {
+    const newPathName = updateSearchParams(title, e.value.toLowerCase());
+    
+    router.push(newPathName);
+  }
+
+
+
+
 
   return (
     <div className="w-fit">
       <Listbox
         value={selected}
-        onChange={(e) => setSelected(e)}
+        onChange={(e) => {
+            setSelected(e);
+            handleUpdateParams(e);
+          }
+        }
       >
 
         <div className="relative w-fit z-10">
@@ -40,9 +56,13 @@ const CustomFilter = ({ title, options }: CustomFilterProps) => {
           >
             <Listbox.Options className="custom-filter__options">
               {options.map((option) => (
-                <Listbox.Option>
+                <Listbox.Option
+                  key={option.title}
+                  value={option}
+                  className={({ active }) => `relative cursor-default select-none py-2 px-4 ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}`}
+                >
                   {({ selected }) => (
-                    <span>
+                    <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>
                       {option.title}
                     </span>
                   )}
